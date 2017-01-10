694b85154acb871b71b9301ce5c97759fc9ddced
heiets <sanomchik@gmail.com>
2017-01-10 15:20:41 +0200

    'deleted-useless-strings'

--- js/app.js
+++ js/app.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-//require('./components/App.scss');
-
 const app = document.getElementById('root');
 ReactDOM.render(<App/>, app);
